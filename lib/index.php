b5e31fb6d1e9898f0e640349d13b718e6426c448
Cédric Belin <cedric@belin.io>
2017-07-19 00:33:27 +0200

    Added the `which` function

--- lib/index.php
+++ lib/index.php
@@ -5,11 +5,19 @@ namespace which;
 use Rx\{Observable};
 
 /**
- * Created by IntelliJ IDEA.
- * User: Cedric
- * Date: 18/07/2017
- * Time: 13:27
+ * Finds the first instance of an executable in the system path.
+ * @param string $command The command to be resolved.
+ * @param bool $all Value indicaing whether to return all found executables, instead of just the first one.
+ * @param array $options The options to be passed to the finder.
+ * @return Observable A string, or an array of strings, specifying the path(s) of the found executable(s).
  */
-function which(string $command, bool $all = false): Observable {
-  return (new Finder)->resolve($command, $all);
+function which(string $command, bool $all = false, array $options = []): Observable {
+  $finder = new Finder(
+    $options['path'] ?? '',
+    $options['extensions'] ?? '',
+    $options['pathSeparator'] ?? ''
+  );
+
+  $executables = $finder->find($command);
+  return $all ? $executables->toArray() : $executables->take(1);
 }
