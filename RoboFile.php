2be69e10313cf42bfd8cf58ecea500a2495258c5
Cédric Belin <cedric@belin.io>
2019-04-26 17:11:01 +0200

    Moved the sources to the 'lib' folder

--- RoboFile.php
+++ RoboFile.php
@@ -115,7 +115,7 @@ class RoboFile extends Tasks {
   function watch(): Result {
     $this->build();
     return $this->taskWatch()
-      ->monitor('src', function() { $this->build(); })
+      ->monitor('lib', function() { $this->build(); })
       ->monitor('test', function() { $this->test(); })
       ->run();
   }
--- doc/usage/api.md
+++ doc/usage/api.md
@@ -1,5 +1,5 @@
 path: blob/master
-source: src/which.php
+source: lib/which.php
 
 # Application programming interface
 This package provides a single function, `Which\which()`, allowing to locate a command in the system path:
