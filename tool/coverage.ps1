b666ad36c750096b40200c9eefef1d02fab00647
Cédric Belin <cedric@belin.io>
2020-05-08 14:30:52 +0200

    Fixed the "coverage" task

--- tool/coverage.ps1
+++ tool/coverage.ps1
@@ -3,5 +3,4 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 $composer = $IsWindows ? 'php "C:\Program Files\PHP\share\composer.phar"' : 'composer'
-$basedir = (Invoke-Expression "$composer global config bin-dir --absolute") -replace '\\', '/'
-Invoke-Expression "'$basedir/coveralls' var/coverage.xml"
+Invoke-Expression "$composer global exec coveralls var/coverage.xml"
