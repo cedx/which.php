282a3f9d762b40df67c9d09aac96f892102ebc05
Cédric Belin <cedric@belin.io>
2021-11-09 17:33:20 +0100

    Updated the project URL

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -3,6 +3,5 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 phpdoc --config=etc/phpdoc.xml
-if (-not (Test-Path docs/api/images)) { New-Item docs/api/images -ItemType Directory | Out-Null }
-Copy-Item doc/img/favicon.ico docs/api/images
-mkdocs build --config-file=etc/mkdocs.yaml
+if (-not (Test-Path docs/images)) { New-Item docs/images -ItemType Directory | Out-Null }
+Copy-Item doc/img/favicon.ico docs/images
