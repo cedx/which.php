71f73dd62137e4ea1ea6ee9b27c9b4c9527dafb8
Cédric Belin <cedric@belin.io>
2021-11-12 19:59:25 +0100

    Updated the dependencies

--- tool/doc.ps1
+++ tool/doc.ps1
@@ -4,4 +4,4 @@ Set-Location (Split-Path $PSScriptRoot)
 
 phpdoc --config=etc/phpdoc.xml
 if (-not (Test-Path docs/images)) { New-Item docs/images -ItemType Directory | Out-Null }
-Copy-Item doc/img/favicon.ico docs/images
+Copy-Item www/favicon.ico docs/images
