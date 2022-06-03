5140f6479230821874e74089ab39f7eb63fe04be
Cédric Belin <cedric@belin.io>
2022-06-03 10:52:13 +0200

    Updated the example

--- example/main.php
+++ example/main.php
@@ -1,17 +1,12 @@
 <?php declare(strict_types=1);
 
-use Which\FinderException;
 use function Which\which;
 
-/** Finds the instances of an executable. */
-function main(): void {
-	try {
-		// `$path` is the absolute path to the executable.
-		$path = which("foobar");
-		print "The command 'foobar' is located at: $path";
-	}
+try {
+	$path = which("foobar")->first(throwIfNotFound: true);
+	print "The command 'foobar' is located at: $path";
+}
 
-	catch (FinderException $e) {
-		print "The command '{$e->getCommand()}' was not found";
-	}
+catch (UnderflowException $e) {
+	print "The command 'foobar' has not been found.";
 }
