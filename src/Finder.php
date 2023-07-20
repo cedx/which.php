b1b245f977b52bc059355cd0a44d9fbaceb42784
Cédric Belin <cedric@belin.io>
2023-07-20 22:24:27 +0200

    Code formatting

--- src/Finder.php
+++ src/Finder.php
@@ -32,11 +32,11 @@ final class Finder {
 
 		if (!$paths) {
 			$pathEnv = getenv("PATH") ?: "";
-			if ($pathEnv) $paths = $pathEnv ? explode(self::isWindows() ? ";" : PATH_SEPARATOR, $pathEnv) : [];
+			$paths = $pathEnv ? explode(self::isWindows() ? ";" : PATH_SEPARATOR, $pathEnv) : [];
 		}
 
 		$this->extensions = array_map(mb_strtolower(...), $extensions);
-		$this->paths = array_filter(array_map(fn(string $directory) => trim($directory, '"'), $paths));
+		$this->paths = array_filter(array_map(fn($item) => trim($item, '"'), $paths));
 	}
 
 	/**
