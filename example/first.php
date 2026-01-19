16de3b436d8b8fb61cecb9e01796652eeb9f14a9
Cédric Belin <cedx@outlook.com>
2026-01-19 22:44:44 +0100

    Update an example [skip ci]

--- example/first.php
+++ example/first.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 use function Belin\Which\which;
 
+// Finds the first instance of an executable.
 $path = which("foobar")->first;
 if (!$path) print "The 'foobar' command cannot be found.";
 else print "The 'foobar' command is located at: $path";
